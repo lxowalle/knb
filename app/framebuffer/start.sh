8d98ea92de7675862f92d2382bfc2a6072f9ecd5
lxowalle <lxowalle@outlook.com>
2021-10-29 09:32:10 +0800

    add framebuffer

---
+++ app/framebuffer/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PJSIP_PATH=../../components/third_party/pjsip
+PJSIP_LIB_PATH=${PJSIP_PATH}/pjproject/lib
+
+export LD_LIBRARY_PATH=${PJSIP_LIB_PATH}:${LD_LIBRARY_PATH}
+
+echo $LD_LIBRARY_PATH
+
+CURR_DIR=`pwd`
+cd $CURR_DIR/build && make -j8 && cd $CURR_DIR
+./build/demo
